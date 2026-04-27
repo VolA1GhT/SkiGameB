626a4d3edaa4751c8624ffb243817a1aded39997
VolA1GhT <dundurs1.4@gmail.com>
2026-04-27 15:47:48 +0300

    PedejaisStundasDarbs

---
+++ SkiGameB/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    private DateTime raceStart;
+    private TimeSpan raceTime;
+    private bool racing = false;
+
+
+    public delegate void TimerEvent();
+
+    private void OnEnable()
+    {
+        StartGate.StartRace += OnRaceStart;
+        FinishGate.FinishRace += OnRaceFinish;
+    }
+
+    void OnRaceStart()
+    {
+        racing = true;
+        raceStart = DateTime.Now;
+        Debug.Log("Race Starter");
+    }
+
+    void OnRaceFinish()
+    {
+        racing = false;
+        Debug.Log("Race Finished");
+    }
+    private void Update()
+    {
+        if (racing)
+            raceTime = DateTime.Now - raceStart;
+        Debug.Log("Race Time " + raceTime);
+    }
+}
